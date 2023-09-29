1adc34ffad953aaa2aebb02101bd62d640d43368
Todd Tarsi <tarsitodd@gmail.com>
2023-09-28 20:13:08 -0500

    Create logInspector.d.ts

---
+++ types/selenium-webdriver/bidi/logInspector.d.ts
@@ -0,0 +1,24 @@
+import { FilterBy } from './filterBy';
+import { ConsoleLogEntry, JavascriptLogEntry, GenericLogEntry } from './logEntries';
+
+class LogInspector {
+  private _driver: any;
+  private _browsingContextIds: any;
+  public listener: Record<string, Function[]>;
+  public bidi: any;
+  public ws: { on: Function };
+
+  constructor(driver: any, browsingContextIds: any);
+
+  init(): Promise<void>;
+  logListener(kind: string): void;
+  onConsoleEntry(callback: (entry: ConsoleLogEntry) => void, filterBy?: FilterBy): Promise<void>;
+  onJavascriptLog(callback: (entry: JavascriptLogEntry) => void, filterBy?: FilterBy): Promise<void>;
+  onJavascriptException(callback: (entry: JavascriptLogEntry) => void): Promise<void>;
+  onLog(callback: (entry: ConsoleLogEntry | JavascriptLogEntry | GenericLogEntry) => void, filterBy?: FilterBy): Promise<void>;
+  close(): Promise<void>;
+}
+
+const getLogInspectorInstance: (driver: any, browsingContextIds: any) => Promise<LogInspector>;
+
+export = getLogInspectorInstance;
